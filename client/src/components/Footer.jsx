ae147cf118728901b68bff5d0b09e3468801ce02
Armando Mora <armandomora14115@gmail.com>
2025-09-06 14:22:55 -0500

    Adecuando estilos finales y alistando para realizar cambios y definir el contenido

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,9 @@ export default function Footer(){
 return (
 <footer className="footer">
 <div className="container footer-inner">
-<span>© {new Date().getFullYear()} Tu Nombre</span>
-<a href="https://github.com/tuuser" target="_blank" rel="noreferrer">GitHub</a>
-<a href="https://www.linkedin.com/in/tuuser" target="_blank" rel="noreferrer">LinkedIn</a>
+<span>© {new Date().getFullYear()} Armando Mora</span>
+<a href="https://github.com/98141/" target="_blank" rel="noreferrer">GitHub</a>
+<a href="https://www.linkedin.com/in/mbt/" target="_blank" rel="noreferrer">LinkedIn</a>
 </div>
 </footer>
 );
